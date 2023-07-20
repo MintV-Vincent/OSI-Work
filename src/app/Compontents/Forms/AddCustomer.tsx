5707a7d638a675c8e87c047c7d1df32cb3b231e3
Vincent Tran <vtran@pfcflex.com>
2023-07-20 15:52:20 -0400

    anotehr commit

--- src/app/Compontents/Forms/AddCustomer.tsx
+++ src/app/Compontents/Forms/AddCustomer.tsx
@@ -43,6 +43,50 @@ export default function AddCustomerForm({ handleClick }: AddCustomerInterface) {
               createData(upperCaseString(values.customer), code)
             );
             if (itemAdded === 0) {
+              setCode(code);
+              setCustomer(upperCaseString(values.customer));
+              handleClick();
+            }
+            values.customer = "";
+          }
+        })}
+      >
+        <TextInputForm form={form} text="Customer" />
+        <Group position="right" mt="md">
+          <Button type="submit" className="">
+            Submit
+          </Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
+
+/*
+export default function AddCustomerForm({ handleClick }: AddCustomerInterface) {
+  const [, setCustomer] = useAtom(customerAtom);
+  const [, setCode] = useAtom(codeAtom);
+  const [customerRow] = useAtom(customerRowAtom);
+  const form = useForm<{
+    customer: string;
+  }>({
+    initialValues: { customer: "" },
+    validate: (values) => ({
+      customer: values.customer.length < 1 ? "Invalid Customer" : null,
+    }),
+  });
+
+  return (
+    <Box maw={340} mx="auto">
+      <form
+        onSubmit={form.onSubmit((values) => {
+          if (form && values.customer != "") {
+            const code: string = createCode(customerRow);
+            const itemAdded: number = addItem(
+              customerRow,
+              createData(upperCaseString(values.customer), code)
+            );
+            if (itemAdded === 0) {
               SuccessfulNotification({
                 title: "Customer Added",
                 message: code,
@@ -70,3 +114,5 @@ export default function AddCustomerForm({ handleClick }: AddCustomerInterface) {
     </Box>
   );
 }
+
+*/
